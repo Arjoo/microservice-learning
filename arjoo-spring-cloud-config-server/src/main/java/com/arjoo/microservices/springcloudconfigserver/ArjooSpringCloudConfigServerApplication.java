f9dddcc91c9df7dd81bb9f1e2e7e535fe9552d58
Arjoo <2009arjoo@gmail.com>
2021-04-11 13:24:31 +0530

    Added init of arjoo-spring-cloud-config-server

---
+++ arjoo-spring-cloud-config-server/src/main/java/com/arjoo/microservices/springcloudconfigserver/ArjooSpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.arjoo.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjooSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjooSpringCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ arjoo-spring-cloud-config-server/src/test/java/com/arjoo/microservices/springcloudconfigserver/ArjooSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjoo.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjooSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
